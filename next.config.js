f3c4cef82784105e7ef5ef9457b50138cfafe124
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-06-15 23:18:58 +0530

    sidebar complete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_MORALIS_SERVER: process.env.NEXT_MORALIS_SERVER,
+    NEXT_PUBLIC_MORALIS_APP_ID: process.env.NEXT_PUBLIC_MORALIS_APP_ID,
+  },
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "openseauserdata.com",
+      "brand.assets.adidas.com",
+      "media0.giphy.com",
+      "avatars.dicebear.com",
+      "media1.giphy.com",
+      "media3.giphy.com",
+      "media2.giphy.com",
+      "media4.giphy.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
+// import { AmazonProvider } from "../context/AmazonContext";
+
+//ReactMoralisError: Provide a "appId" provided to <MoralisProvider>
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_MORALIS_SERVER}
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    >
+      {/* <AmazonProvider> */}
+        <Component {...pageProps} />
+      {/* </AmazonProvider> */}
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
