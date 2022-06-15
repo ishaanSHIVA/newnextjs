887b6d406b817cd93f3b42c2f8cde5ce54fd8a14
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-06-15 23:55:17 +0530

    import

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-// import Main from "../components/Main.jsx";
-// import Sidebar from "../components/SideBar.jsx";
-
 const styles = {
   container: "h-full w-full flex bg-[#fff]",
 };
 export default function Home() {
+  // import Main from "../components/Main.jsx";
+  import Sidebar from "../components/SideBar.jsx";
+
   return (
     <div className={styles.container}>
       <h1>HI</h1>
-      {/* <Sidebar /> */}
+      <Sidebar />
       {/* <Main /> */}
     </div>
   );
